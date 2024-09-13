a10c0f4228dce0da3fd7b5d3b81859fc9bf3b870
Florensa Dimer <flofloor@gmail.com>
2024-09-13 18:50:06 -0300

    image converted

--- converte_img.py
+++ converte_img.py
@@ -9,10 +9,9 @@ print(df.columns)
 # Iterar sobre as linhas do DataFrame
 for index, row in df.iterrows():
     img_bytes = eval(row['ARQUIVO'])  # 'eval' converte a string de bytes para bytes reais
-    # Seu código para processar img_bytes
-    
+    aux_value = row['AUX'].strip()
         # Passo 3: Salvar a imagem
-    with open(f'C:\\Users\\Dell\\OneDrive - PUCRS - BR\\7 Semestre 2024-2\\8 Projeto em Ciência de Dados I\\Projeto NetEye\\dados\\images\\img_{index+1}.png', 'wb') as img_file:
+    with open(f'C:\\Users\\Dell\\OneDrive - PUCRS - BR\\7 Semestre 2024-2\\8 Projeto em Ciência de Dados I\\Projeto NetEye\\dados\\images\\img_{index+1}_{aux_value}.png', 'wb') as img_file:
         img_file.write(img_bytes)
 
 print("Imagens convertidas e salvas.")
