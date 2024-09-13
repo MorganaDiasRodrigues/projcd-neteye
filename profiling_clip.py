40ee89b9d7d80a8a5656f32eef97d5d817d131bb
Florensa Dimer <flofloor@gmail.com>
2024-09-13 18:16:13 -0300

    some results

--- profiling_clip.py
+++ profiling_clip.py
@@ -2,7 +2,7 @@ from ydata_profiling import ProfileReport
 import pandas as pd
 
 # Substitua 'seu_arquivo.csv' pelo caminho para o seu arquivo CSV
-df = pd.read_csv('image_text_similarity_results.csv')
+df = pd.read_csv('clip_results.csv')
 
 # Crie o relatório
 profile = ProfileReport(df, title="Relatório de Profiling", explorative=True)
