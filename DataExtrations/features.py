287579e9903d3f270cb0d457f4292251912006e0
Morgana Rodrigues <morganadiasrodrigues@outlook.com>
2024-10-18 12:07:42 -0300

    prod notebook

--- DataExtrations/features.py
+++ DataExtrations/features.py
@@ -10,10 +10,8 @@ def dia_da_semana(bi_sessoes):
 
 def transactions(df):
 
-    # Create a 'group' column to identify consecutive groups with the same ID_STATUS_PROGRAMA
     df['group'] = (df['ID_STATUS_PROGRAMA'] != df['ID_STATUS_PROGRAMA'].shift()).cumsum()
 
-    # Define the function to sum QT_MINUTO within each group and keep ID_STATUS_PROGRAMA
     df = df.groupby(['group', 'ID_STATUS_PROGRAMA']).apply(
         lambda x: pd.Series({
             'DT_ACESSO': x['DT_ACESSO'].iloc[0],  # Taking the first date for the group
@@ -23,20 +21,15 @@ def transactions(df):
         })
     ).reset_index(drop=True)
 
-    # Calculate the shift of ID_STATUS_PROGRAMA to identify transitions
     df['NEXT_STATUS'] = df['ID_STATUS_PROGRAMA'].shift(-1)
     df['NEXT_QT_MINUTO'] = df['QT_MINUTO'].shift(-1)
 
-    # Filter out rows where NEXT_STATUS is NaN (last row of each day)
     df = df.dropna(subset=['NEXT_STATUS'])
 
-    # Create a transition column
     df['TRANSITION'] = df['ID_STATUS_PROGRAMA'].astype(str) + ' to ' + df['NEXT_STATUS'].astype(str)
 
-    # Calculate the QT_MINUTO duration for each transition
     df['DURATION'] = df['QT_MINUTO']
 
-    # Group by date and transition to calculate the mean duration
     return df.groupby(['DT_ACESSO', 'TRANSITION'])['DURATION'].mean().unstack(fill_value=0)
 
 def numero_de_programas():
